877db88bf74523c428c2504b33d3da408e4535e2
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-08-02 10:12:19 -0400

    GifGrid - Nuevo componente

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { AddCategory } from "./components/AddCategory"
+import { GifGrid } from "./components/GifGrid"
 
 export const GifExpertApp = () => {
 
@@ -19,13 +20,11 @@ export const GifExpertApp = () => {
       
       <AddCategory onAddCategory={ onAddCategory } />
       
-      <ol>
-        {
-          categories.map( ( category, i ) => {
-            return <li key={ i }> { category } </li>
-          })
-        }
-      </ol>
+      {
+        categories.map( ( category ) => (
+            <GifGrid key={ category } category={ category } />
+          ))
+      }
     
     </>
   )
